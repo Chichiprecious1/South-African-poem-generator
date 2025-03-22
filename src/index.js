cdbe755f66f48e8c7843a337caa8b93c22fb73c5
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-03-22 15:48:33 -0400

    added AI API intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("poem generated");
 
   new Typewriter("#poem", {
-    strings: " Our own shadows disappear as the feet of thousands",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "6b9ecc0tb4b57ae4e51e839o1046fad3";
+  let context =
+    "You are an expect at peaceful poem writering and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br/>. Make sure to follow the user instructions. sign the poem with SheCodes AI inside a <strong> element";
+  let prompt = `User instructions: Generate a South African poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`prompt: ${prompt}`);
+  console.log(`contex: ${context}`);
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
