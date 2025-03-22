db1d9de14176d69af4b5d74c48fb0c618708fc78
Chichiprecious1 <mulibanatshilidzi11@gmail.com>
2025-03-22 14:02:53 -0400

    added basic form handling and added Typewriter pluggin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: " Our own shadows disappear as the feet of thousands",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
